8b9ca95bbff281340675dfce1eb73fccbd8140ef
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-08-08 12:14:12 +0100

    added random answer options to question

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { countriesData } from "./data/countriesData";
-import { randomRange } from "./functions/generateRandomNumber";
+import { getRandomNumber } from "./functions/getRandomNumber";
 import "./main.scss";
 
-const questionsData = countriesData[0];
-let totalScore: number = 0;
-let maxPointsSoFar: number = 0;
-let score: number = 0;
-let isPlayButtonClicked = false;
+// let totalScore: number = 0;
+// let score: number = 0;
+let nextButtonCounter = 1;
 
 const rulesDescription = document.querySelector(
   ".rules__description"
@@ -20,28 +18,66 @@ const questionContainer = document.querySelector(
 const answersContainer = document.querySelector(
   ".answers-container"
 ) as HTMLElement;
+const nexQuestionButton = document.querySelector(
+  ".question__button"
+) as HTMLButtonElement;
 
-if (!rulesDescription || !playButton || !questionContainer || !answersContainer)
+if (
+  !rulesDescription ||
+  !playButton ||
+  !questionContainer ||
+  !answersContainer ||
+  !nexQuestionButton
+)
   throw new Error("Issue with selector");
 
+nexQuestionButton.style.display = "none";
+
 const handleClickOnPlayButton = () => {
-  isPlayButtonClicked = true;
+  playButton.style.display = "none";
+  nexQuestionButton.style.display = "";
   rulesDescription.innerHTML = "";
-  playButton.innerHTML = "Next question!";
-  questionContainer.innerHTML = `<div class="question-container__image"><img src="${questionsData.flag}" alt="The flag of ${questionsData.name}" class="question-container__image" /></div><p class="question-container__question">Which answer is closest to ${questionsData.name}'s population?</p>`;
 
-  const multipliers: number[] = randomRange(5, 15, 4);
+  // generate radom country question
+  let countryIndex: number = getRandomNumber(0, 249);
+  questionContainer.innerHTML = `<div class="question-container__image"><img src="${countriesData[countryIndex].flag}" alt="The flag of ${countriesData[countryIndex].name}" class="question-container__image" /></div><p class="question-container__question">Which answer is closest to ${countriesData[countryIndex].name}'s population?</p>`;
 
-  if (isPlayButtonClicked) {
-    answersContainer.innerHTML += `<p class="answers-container__total-score">Your total score: ${totalScore}/${maxPointsSoFar} points</p><p class="answers-container__last-score">${score} points for the last guess</p>`;
-    multipliers.forEach((multiplier) => {
-      answersContainer.innerHTML += `<button class="answers-container__answer">${Math.floor(
-        (questionsData.population * multiplier) / 10
-      )}</button>`;
-    });
-  } else {
-    answersContainer.innerHTML = ``;
+  // generate random answer options to the question
+  const startingMultiplier: number = getRandomNumber(5, 12);
+  for (let i = 0; i < 4; i++) {
+    answersContainer.innerHTML += `<button class="answers-container__answer">${Math.floor(
+      ((startingMultiplier + i) / 10) * countriesData[countryIndex].population
+    )}</button>`;
+  }
+};
+
+const handleClickOnNextButton = () => {
+  nextButtonCounter += 1;
+  if (nextButtonCounter === 9) nexQuestionButton.style.display = "none";
+
+  // generate next radom country question
+  questionContainer.innerHTML = "";
+  let countryIndex: number = getRandomNumber(0, 249);
+  questionContainer.innerHTML += `<div class="question-container__image"><img src="${countriesData[countryIndex].flag}" alt="The flag of ${countriesData[countryIndex].name}" class="question-container__image" /></div><p class="question-container__question">Which answer is closest to ${countriesData[countryIndex].name}'s population?</p>`;
+
+  // generate random answer options to the question
+
+  answersContainer.innerHTML = "";
+  const startingMultiplier: number = getRandomNumber(5, 12);
+  for (let i = 0; i < 4; i++) {
+    answersContainer.innerHTML += `<button class="answers-container__answer">${Math.floor(
+      ((startingMultiplier + i) / 10) * countriesData[countryIndex].population
+    )}</button>`;
   }
 };
 
 playButton.addEventListener("click", handleClickOnPlayButton);
+nexQuestionButton.addEventListener("click", handleClickOnNextButton);
+
+// const handleClickOnAnswerButton = () => {
+//   answersContainer.innerHTML =
+//     `<p class="answers-container__total-score">Your total score: ${totalScore} points</p><p class="answers-container__last-score">${score} points for the last guess</p>` +
+//     answersContainer.innerHTML;
+//   console.log("something");
+// };
+// answersContainer.addEventListener("click", handleClickOnAnswerButton);
